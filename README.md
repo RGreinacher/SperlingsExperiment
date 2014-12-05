d1791952a6a7a4f2502b2ad3804d99655dc3e09d
Robert Greinacher <robert.p.greinacher@campus.tu-berlin.de>
2014-12-05 02:03:15 +0100

    initial project files

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ SperlingsExperiment
 ===================
 
 WebApp simulation of Prof. Dr. Sperlings experiment to the capabilities of the ultra short-term memory.
+
+Just open the `index.html` file for launching.
+
+The WebApp is currently german only - it will be translated.
---
+++ js/main.js
@@ -0,0 +1,135 @@
+var TIME_TO_SHOW_HIGHLIGHTED_INPUTS = 2000;
+var DURATION_MATRIX_IS_VISIBLE = 50;
+var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lettersInMatrix = [[], [], []];
+var challengeRow;
+var roundsPlayed = 0;
+var roundsWon = 0;
+
+$('#no-js-warning').remove();
+resetSperlingsExperiment();
+
+$('#startButton').click(function (event) {
+    startSperlingsExperiment();
+    event.preventDefault();
+});
+
+$('#resultButton').click(function (event) {
+    showSolution();
+    event.preventDefault();
+});
+
+function resetSperlingsExperiment() {
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 4; j++) {
+            $('#row' + i + 'letter' + j).unbind();
+            $('#row' + i + 'letter' + j).removeClass('highlighted-input');
+            $('#row' + i + 'letter' + j).addClass('default-input');
+
+            randomLetter = Math.floor(Math.random() * 26);
+            $('#row' + i + 'letter' + j).val(alphabet[randomLetter]);
+            lettersInMatrix[i][j] = alphabet[randomLetter];
+        }
+    }
+
+    hideMatrix();
+    $('#resultButton').css('visibility', 'hidden');
+    $('#anchorman').html('+');
+    $('#anchorman').css('color', '#FFFFFF');
+}
+
+function startSperlingsExperiment() {
+    resetSperlingsExperiment();
+    $('#anchorman').css('color', '#CCCCCC');
+
+    setTimeout(function () {
+        $('#anchorman').css('color', '#AAAAAA');
+
+        setTimeout(function () {
+            $('#anchorman').css('color', '#777777');
+
+            setTimeout(function () {
+                showMatrixFor50ms();
+            }, 1000);
+
+        }, 1000);
+
+    }, 1000);
+}
+
+function showMatrix() {
+    $('#anchorman').css('display', 'none');
+    $('.row0-letters, .row1-letters, .row2-letters').css('visibility', 'visible');
+    $('.row1-letters').css('display', 'block');
+}
+
+function hideMatrix() {
+    $('.row0-letters, .row1-letters, .row2-letters').css('visibility', 'hidden');
+    $('.row1-letters').css('display', 'none');
+    $('#anchorman').css('display', 'block');
+}
+
+function hideLettersAndAnchorman() {
+    $('.row0-letters, .row1-letters, .row2-letters').css('visibility', 'hidden');
+    $('.row1-letters').css('display', 'block');
+    $('#anchorman').css('display', 'none');
+}
+
+function clearMatrix() {
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 4; j++) {
+            $('#row' + i + 'letter' + j).val('');
+        }
+    }
+}
+
+function showMatrixFor50ms() {
+    showMatrix();
+    setTimeout(function () {
+        hideMatrix();
+        $('#anchorman').html('&nbsp;'); // ;o)
+
+        setTimeout(function () {
+            askForLetters();
+        }, TIME_TO_SHOW_HIGHLIGHTED_INPUTS);
+
+    }, DURATION_MATRIX_IS_VISIBLE);
+}
+
+function askForLetters() {
+    challengeRow = Math.floor(Math.random() * 3);
+
+    clearMatrix();
+    hideLettersAndAnchorman();
+
+    $('.row' + challengeRow + '-letters').css('visibility', 'visible');
+    for (i = 0; i < 4; i++) {
+        $('#row' + challengeRow + 'letter' + i).removeClass('default-input').addClass('highlighted-input');
+    }
+
+    $('#row' + challengeRow + 'letter0').focus();
+    $('#resultButton').css('visibility', 'visible');
+}
+
+function showSolution() {
+    var allInputsAreCorrect = true;
+
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 4; j++) {
+            if (i == challengeRow && $('#row' + challengeRow + 'letter' + j).val().toUpperCase() != lettersInMatrix[challengeRow][j]) {
+                allInputsAreCorrect = false;
+            }
+            $('#row' + i + 'letter' + j).val(lettersInMatrix[i][j]);
+        }
+    }
+
+    showMatrix();
+    roundsPlayed += 1;
+
+    if (allInputsAreCorrect) {
+        roundsWon += 1;
+        alert('Toll! Alles richtig!\nKorrekt erinnert: ' + roundsWon + ' von ' + roundsPlayed);
+    } else {
+        alert('Leider nicht ganz richtig.\nKorrekt erinnert: ' + roundsWon + ' von ' + roundsPlayed);
+    }
+}
